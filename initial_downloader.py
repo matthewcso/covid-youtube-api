bd390e1801081ab6baa11978f9eda200eb373f6d
Apeirogons <som5@mcmaster.ca>
2021-12-07 11:33:57 -0600

    Good night

--- initial_downloader.py
+++ initial_downloader.py
@@ -14,23 +14,34 @@ if not os.path.exists(output_dir):
 
 search_query = 'coronavirus|covid|wuhan'
 i=0
+overall_time_range = time_range_maker('Oct 18, 2020', datetime.now() - pd.Timedelta(7, 'days'))
 
-overall_time_range = time_range_maker('Aug 01, 2021', datetime.now() - pd.Timedelta(7, 'days'))
+#overall_time_range = time_range_maker('Dec 29, 2019', datetime.now() - pd.Timedelta(7, 'days'))
+
+
+DEVELOPER_KEY = devkeys[i]
+youtube = googleapiclient.discovery.build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+        developerKey=DEVELOPER_KEY)
+i += 1
 
 for week_n, time_range in enumerate(overall_time_range):
-    # Switch developer key due to usage limitations - not a problem in Aug 2020 but would be a problem for future
-    if week_n % 45 == 0: 
+    def run():
+        print(time_range)
+        if (pd.to_datetime(time_range[0]).month == 12 or pd.to_datetime(time_range[0]).month == 1) and (pd.to_datetime(time_range[0]).year < 2021):
+            sq = search_query + '|SARS'
+        else:
+            sq = search_query
+        output_df, request_info = video_information_from_search(sq, youtube, time_range=time_range, regionCode='ca', max_results=30)
+        output_df.to_csv(output_dir+'/'+time_range[0][:10]+'__'+ time_range[1][:10] + '.csv')
+
+    try:    # Switch developer key due to usage limitations - not a problem in Aug 2020 but would be a problem for future
+        run()
+
+    except HttpError:
         DEVELOPER_KEY = devkeys[i]
         youtube = googleapiclient.discovery.build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                 developerKey=DEVELOPER_KEY)
         i += 1
-
-    print(time_range)
-
-    if (pd.to_datetime(time_range[0]).month == 12 or pd.to_datetime(time_range[0]).month == 1) and (pd.to_datetime(time_range[0]).year < 2021):
-        sq = search_query + '|SARS'
-    output_df, request_info = video_information_from_search(sq, youtube, time_range=time_range, regionCode='ca')
-    output_df.to_csv(output_dir+'/'+time_range[0][:10]+'__'+ time_range[1][:10] + '.csv')
-
+        run()
 
 # %%
